68ca74872f7651bca5039d01b7e5f57faf8547d4
gaurav-proglint <Gauravprabhakar.s@proglint.com>
2024-03-15 13:58:37 +0530

    token updation

--- crud.py
+++ crud.py
@@ -6,7 +6,13 @@ def get_user(db:session,username:str):
     return db.query(models.User).filter(models.User.username==username).first()
 
 def insert_token(db:session,id:int,token:str):
-    new_token = models.Token(id=id, token=token)
-    db.add(new_token)
-    db.commit()
-    db.refresh(new_token)
+    user=db.query(models.Token).filter(models.Token.id==id).first()
+    if user:
+        user.token = token
+        db.commit()
+    else:    
+        new_token = models.Token(id=id, token=token)
+        db.add(new_token)
+        db.commit()
+        db.refresh(new_token)
+
